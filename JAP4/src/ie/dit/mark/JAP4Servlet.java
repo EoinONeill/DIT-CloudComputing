b3adcacfd285f2397d8bed9c9759ddbddc64861a
Mark Deegan <mark@dit.ie>
2012-05-12 21:02:08 +0100

    Building GIT repository in Eclipse

---
+++ JAP4/src/ie/dit/mark/JAP4Servlet.java
@@ -0,0 +1,11 @@
+package ie.dit.mark;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JAP4Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
